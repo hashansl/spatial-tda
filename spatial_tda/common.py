7e792912eef193d210f8bc4f51b86ae9cec90a58
Hashan Dananjaya <hashan.dan@gmail.com>
2025-02-12 20:37:47 -0500

    yml updated and added adjacency_simplex.md

--- spatial_tda/common.py
+++ spatial_tda/common.py
@@ -5,3 +5,17 @@ def hello_world():
     """Prints "Hello World!" to the console.
     """
     print("Hello World!")
+
+
+def add(a, b):
+    """Adds two numbers together.
+
+    Args:
+        a (int): The first number.
+        b (int): The second number.
+
+    Returns:
+        int: The sum of the two numbers.
+    """
+    return a + b
+
