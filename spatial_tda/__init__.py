6ca5e6a6ac8560039c4710c757f8734acd286210
Hashan Dananjaya <hashan.dan@gmail.com>
2025-02-13 23:20:07 -0500

    package structure changed

--- spatial_tda/__init__.py
+++ spatial_tda/__init__.py
@@ -4,4 +4,6 @@ __author__ = """Hashan Fernando"""
 __email__ = "hashan.dan@gmail.com"
 __version__ = "0.1.2"
 
-from .adjacency_simplex import *
+# from .adjacency_simplex import *
+from .adjacency_simplex import AdjacencySimplex
+
